5a53af68bbcc445857598eb89accb02b251332d0
Adm1RalDen <games.zadrotina@gmail.com>
2022-11-16 21:47:53 +0200

    fix: corrected issue with test

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 
 import Header from '@/components/layout/Header';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({
+  children,
+  isRenderHeader = true,
+}: {
+  children: React.ReactNode;
+  isRenderHeader?: boolean;
+}) {
   // Put Header or Footer Here
   return (
     <>
-      <Header />
+      {isRenderHeader && <Header />}
       {children}
     </>
   );
